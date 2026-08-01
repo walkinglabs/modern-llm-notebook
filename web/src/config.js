05fa48d6de530ef4b6dd792c3ff9f69aa0580940
sanbuphy <physicoada@gmail.com>
2026-08-01 13:36:45 +0800

    feat: enable ModelScope notebook links

--- web/src/config.js
+++ web/src/config.js
@@ -21,32 +21,23 @@ function getNotebookPath(meta, notebookId) {
   return `${getNotebookRoot(meta)}/${meta?.partDir}/${notebookId}.ipynb`
 }
 
-function withNotebookParams(baseUrl, meta, notebookId) {
-  const params = new URLSearchParams({
-    git_repo: GITHUB_REPO_GIT_URL,
-    git_branch: GITHUB_BRANCH,
-    file_path: getNotebookPath(meta, notebookId),
-  })
-  return `${baseUrl}?${params.toString()}`
-}
-
 export function getNotebookModelScopeUrl(meta, notebookId) {
-  return withNotebookParams('https://modelscope.cn/my/mynotebook/preset', meta, notebookId)
+  return `https://modelscope.cn/notebook/share/github/${GITHUB_OWNER}/${GITHUB_REPO}` +
+    `/blob/${GITHUB_BRANCH}/${getNotebookPath(meta, notebookId)}`
 }
 
 export function getNotebookLaunchLinks(meta, notebookId) {
   const isEnglish = meta?.lang === 'en'
   return [
     {
-      id: 'colab',
-      label: isEnglish ? 'Open in Colab' : '在 Colab 打开',
-      href: getNotebookColabUrl(meta, notebookId),
-    },
-    {
       id: 'modelscope',
       label: isEnglish ? 'Open in ModelScope' : '在 ModelScope 打开',
       href: getNotebookModelScopeUrl(meta, notebookId),
-      disabled: true,
+    },
+    {
+      id: 'colab',
+      label: isEnglish ? 'Open in Colab' : '在 Colab 打开',
+      href: getNotebookColabUrl(meta, notebookId),
     },
   ]
 }
